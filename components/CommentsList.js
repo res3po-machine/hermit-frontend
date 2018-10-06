de940c793f06f19dc3c7bcff78ed5ca5126bcb19
res3po-machine <res3po@outlook.com>
2018-10-06 15:11:37 -0700

    Refactered commentslist

--- components/CommentsList.js
+++ components/CommentsList.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import { View, Text, StyleSheet, AsyncStorage } from 'react-native'
-import { Avatar, Card, Divider, ListItem, FormLabel, FormInput, Button } from 'react-native-elements'
-import moment from 'moment'
+import { Avatar, Card, ListItem, FormLabel, FormInput, Button } from 'react-native-elements'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { commentsByTrail, postComment, selectComment } from '../actions/commentActions'
 
-const mapStateToProps = ({trails, comments, users}) => ({trails, comments, users})
+import moment from 'moment'
+
+const mapStateToProps = ({trails, fav_trails, comments}) => ({trails, fav_trails, comments})
 const mapDispatchtoProps = (dispatch) => bindActionCreators({
     commentsByTrail, postComment, selectComment
 }, dispatch)
@@ -20,20 +21,32 @@ class CommentsList extends Component {
         }
     }
 
-    async componentDidMount() {
-        const preToken = await AsyncStorage.getItem('hermitToken')
-        const token = JSON.parse(preToken)
-        this.props.commentsByTrail(this.props.trails.trailSelect, token.token)
+    componentDidMount = () => {
+        this.loadComments()
     }
 
-    async onSubmit() {
+    loadComments = async () => {
+        const token = await this.getToken()
+        await this.props.commentsByTrail(this.props.trails.trailSelect, token.token)
+    }
+
+    getToken = async () => {
         const preToken = await AsyncStorage.getItem('hermitToken')
-        const token = JSON.parse(preToken)
+        return JSON.parse(preToken)
+    }
+
+    onSubmit = async () => {
+        const token = await this.getToken()
         const trailId = this.props.trails.trailSelect
-        const trailName = this.props.trails.data.find(trail => trail.id === trailId).name
-        await this.props.postComment({body: this.state.body, userId: token.id, trailId, trailName }, token.token)
+        if (this.props.trails.data.find(trail => trail.id === trailId)) {
+            const trailName = this.props.trails.data.find(trail => trail.id === trailId).name
+            await this.props.postComment({body: this.state.body, userId: token.id, trailId, trailName }, token.token)
+        } else {
+            const trailName = this.props.fav_trails.full.find(trail => trail.id === trailId).name
+            await this.props.postComment({body: this.state.body, userId: token.id, trailId, trailName }, token.token)
+        }
         this.setState({body: ''})
-        this.props.commentsByTrail(this.props.trails.trailSelect, token.token)
+        this.loadComments()
     }
 
     select = (id) => {
@@ -44,31 +57,30 @@ class CommentsList extends Component {
     render() {
         return (
             <View>
-                <Card containerStyle={{padding: 0}} >
+                <Card 
+                containerStyle={styles.formContainer} >
+
                     <FormLabel for="body">Post a Comment:</FormLabel>
                     <FormInput 
                     multiline={true} 
                     numberOfLines={10}
-                    style={{paddingBottom: 10}} 
-                    inputStyle={{width: 280}}
+                    style={styles.inputContainer} 
+                    inputStyle={styles.inputStyle}
                     onChangeText={(text) => this.setState({body: text})} value={this.state.body} 
                     name="body"/>
                     <Button 
-                        loading={this.props.comments.postLoading}
-                        onPress={() => this.onSubmit()} 
-                        style={{paddingVertical: 10, alignSelf: 'flex-end'}} 
-                        title="SEND" 
-                        buttonStyle={{
-                            backgroundColor: "rgba(255, 171,51, 1)",
-                            width: 100,
-                            height: 45,
-                            borderColor: "transparent",
-                            borderWidth: 0,
-                            borderRadius: 100,
-                          }} />
+                    loading={this.props.comments.postLoading}
+                    onPress={this.onSubmit} 
+                    style={styles.buttonContainer} 
+                    title="SEND" 
+                    buttonStyle={styles.buttonStyle} />
+
                 </Card>
-                <View style={{flexDirection: 'column-reverse', paddingBottom: 10}}>
-                    <Card containerStyle={{padding: 0}} >
+                <View 
+                style={styles.listOrder}>
+                    <Card 
+                    containerStyle={styles.formContainer} >
+
                     {
                         this.props.comments.comments.map((comment, i) => {
                         return (
@@ -76,13 +88,18 @@ class CommentsList extends Component {
                             key={i}
                             onPress={() => this.select(comment.id)}
                             title={
-                                <View style={{paddingLeft: 5}}>
+                                <View 
+                                style={styles.commentText}>
                                     <Text>{comment.body}</Text>
                                 </View>
                             }
                             subtitle={
-                                <View style={{alignSelf: 'flex-end'}}>
-                                    <Text style={{fontSize: 10, color: 'grey'}}>{comment.created_at === comment.updated_at ? `Created ${moment(comment.created_at).startOf(Date.now()).fromNow()}` : `Updated ${moment(comment.updated_at).startOf('day').fromNow()}`}</Text>
+                                <View 
+                                style={styles.subContainer}>
+                                    <Text 
+                                    style={styles.subText}>{comment.created_at === comment.updated_at ? 
+                                        `Created ${moment(comment.created_at).startOf(Date.now()).fromNow()}` : 
+                                        `Updated ${moment(comment.updated_at).startOf('day').fromNow()}`}</Text>
                                 </View>
                             }
                             avatar={
@@ -96,6 +113,7 @@ class CommentsList extends Component {
                         );
                         }).reverse()
                     }
+                    
                     </Card>
                 </View>
             </View>
@@ -104,3 +122,41 @@ class CommentsList extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatchtoProps)(CommentsList)
+
+const styles = StyleSheet.create({
+    formContainer: {
+        padding: 0
+    },
+    inputContainer: {
+        paddingBottom: 10
+    },
+    inputStyle: {
+        width: 280
+    },
+    buttonContainer: {
+        paddingVertical: 10, 
+        alignSelf: 'flex-end'
+    },
+    buttonStyle: {
+        backgroundColor: "rgba(255, 171,51, 1)",
+        width: 100,
+        height: 45,
+        borderColor: "transparent",
+        borderWidth: 0,
+        borderRadius: 100,
+      },
+    listOrder: {
+        flexDirection: 'column-reverse', 
+        paddingBottom: 10
+    },
+    commentText: {
+        paddingLeft: 5
+    },
+    subContainer: {
+        alignSelf: 'flex-end'
+    },
+    subText: {
+        fontSize: 10, 
+        color: 'grey'
+    }
+})
